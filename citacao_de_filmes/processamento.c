80aeac4ca8cf347f91411b722e957ddbd0ad701a
evandroflausinoo <evandro.flausino@ufu.br>
2025-04-12 07:03:34 -0300

    Update processamento.c

--- citacao_de_filmes/processamento.c
+++ citacao_de_filmes/processamento.c
@@ -19,7 +19,6 @@ void normaliza_palavra(char *palavra) {
     palavra[j] = '\0';
 }
 
-//separa a linha do arquivo em citação, filme e ano
 void analisa_linha(const char *linha, InfoCitacao *info) {
     char buffer[1024], *pbuffer;
     char citacao[500], filme[500], ano[24];
@@ -27,21 +26,19 @@ void analisa_linha(const char *linha, InfoCitacao *info) {
 
     pbuffer = strtok(buffer, "\"");
     strcpy(citacao, pbuffer);
-    info->citacao = (char*)malloc((strlen(citacao)+1) * sizeof(char));
+    info->citacao = (char*)malloc((strlen(citacao)+1)*sizeof(char));
     strcpy(info->citacao, citacao);
 
     pbuffer = strtok(NULL, "\"");
     strcpy(filme, pbuffer);
-    info->filme = (char*)malloc((strlen(filme)+1) * sizeof(char));
+    info->filme = (char*)malloc((strlen(filme)+1)*sizeof(char));
     strcpy(info->filme, filme);
 
     pbuffer = strtok(NULL, "\n");
     strcpy(ano, pbuffer);
-    info->ano = (char*)malloc((strlen(ano)+1) * sizeof(char));
-    strcpy(info->ano, ano);
+    info->ano = atoi(pbuffer);
 }
 
-//processa cada linha do arquivo e atualiza as 3 estruturas
 void processa_arquivo(const char *nome_arquivo, VetorBuscaBinaria *vet_busbin, NoArvoreBusca **raiz_arvbus, NoAVL **raiz_avl) {
     char linha[1024];
     long offset;
@@ -50,42 +47,32 @@ void processa_arquivo(const char *nome_arquivo, VetorBuscaBinaria *vet_busbin, N
         perror("Erro ao abrir o arquivo.\n");
         return;
     }
-
     offset = ftell(arquivo);
     while (fgets(linha, sizeof(linha), arquivo)) {
         InfoCitacao info;
         analisa_linha(linha, &info);
         char *quote = info.citacao;
         char *token = strtok(quote, " ");
-
         while (token) {
             normaliza_palavra(token);
             if (strlen(token) > 3) {
-                //vetor com busca binária
-                insere_busbin(vet_busbin, token, 1, offset);
-
-                //arvore binária de busca
-                insere_arvbus(raiz_arvbus, token, 1, offset);
-
-                //arvore AVL alfabética
-                EntradaRepositorio entrada_avl;
-                entrada_avl.palavra = strdup(token);
-                entrada_avl.frequencia = 1;
-                entrada_avl.offsets = malloc(sizeof(long));
-                entrada_avl.offsets[0] = offset;
-                entrada_avl.offset_cont = 1;
-
-                insere_avl_alfabeto(raiz_avl, entrada_avl);
+                EntradaRepositorio entrada;
+                entrada.palavra = strdup(token);
+                entrada.frequencia = 1;
+                entrada.offsets = malloc(sizeof(long));
+                entrada.offsets[0] = offset;
+                entrada.offset_cont = 1;
+                insere_busbin(vet_busbin, entrada);
+                insere_arvbus(raiz_arvbus, entrada);
+                insere_avl(raiz_avl, entrada);
+                free(entrada.palavra);
+                free(entrada.offsets);
             }
             token = strtok(NULL, " ");
         }
-
         free(info.citacao);
         free(info.filme);
-        free(info.ano);
-
         offset = ftell(arquivo);
     }
-
     fclose(arquivo);
 }
