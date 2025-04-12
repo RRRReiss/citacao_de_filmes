fbdb76471384298eda9bef01e07139a2b51ddaf7
evandroflausinoo <evandro.flausino@ufu.br>
2025-04-12 06:39:33 -0300

    Update processamento.c

--- citacao_de_filmes/processamento.c
+++ citacao_de_filmes/processamento.c
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//funÁ„o respons·vel por retirar caracteres que n„o sejam alfanumÈricos (isalnum) e por deixar tudo min˙sculo (tolower)
+//remove caracteres n√£o alfanum√©ricos e converte para min√∫sculo
 void normaliza_palavra(char *palavra) {
     int i = 0, j = 0;
     while (palavra[i]) {
         if (isalnum(palavra[i])) {
-            if(isdigit(palavra[i]))
+            if (isdigit(palavra[i]))
                 palavra[j++] = palavra[i];
             else
                 palavra[j++] = tolower(palavra[i]);
@@ -19,6 +19,7 @@ void normaliza_palavra(char *palavra) {
     palavra[j] = '\0';
 }
 
+//separa a linha do arquivo em cita√ß√£o, filme e ano
 void analisa_linha(const char *linha, InfoCitacao *info) {
     char buffer[1024], *pbuffer;
     char citacao[500], filme[500], ano[24];
@@ -26,20 +27,21 @@ void analisa_linha(const char *linha, InfoCitacao *info) {
 
     pbuffer = strtok(buffer, "\"");
     strcpy(citacao, pbuffer);
-    info->citacao = (char*)malloc((strlen(citacao)+1)*sizeof(char));
+    info->citacao = (char*)malloc((strlen(citacao)+1) * sizeof(char));
     strcpy(info->citacao, citacao);
 
     pbuffer = strtok(NULL, "\"");
     strcpy(filme, pbuffer);
-    info->filme = (char*)malloc((strlen(filme)+1)*sizeof(char));
+    info->filme = (char*)malloc((strlen(filme)+1) * sizeof(char));
     strcpy(info->filme, filme);
 
     pbuffer = strtok(NULL, "\n");
     strcpy(ano, pbuffer);
-    info->ano = (char*)malloc((strlen(ano)+1)*sizeof(char));
+    info->ano = (char*)malloc((strlen(ano)+1) * sizeof(char));
     strcpy(info->ano, ano);
 }
 
+//processa cada linha do arquivo e atualiza as 3 estruturas
 void processa_arquivo(const char *nome_arquivo, VetorBuscaBinaria *vet_busbin, NoArvoreBusca **raiz_arvbus, NoAVL **raiz_avl) {
     char linha[1024];
     long offset;
@@ -48,24 +50,42 @@ void processa_arquivo(const char *nome_arquivo, VetorBuscaBinaria *vet_busbin, N
         perror("Erro ao abrir o arquivo.\n");
         return;
     }
+
     offset = ftell(arquivo);
     while (fgets(linha, sizeof(linha), arquivo)) {
         InfoCitacao info;
         analisa_linha(linha, &info);
         char *quote = info.citacao;
         char *token = strtok(quote, " ");
+
         while (token) {
             normaliza_palavra(token);
             if (strlen(token) > 3) {
+                //vetor com busca bin√°ria
                 insere_busbin(vet_busbin, token, 1, offset);
+
+                //arvore bin√°ria de busca
                 insere_arvbus(raiz_arvbus, token, 1, offset);
-                insere_avl(raiz_avl, token, 1, offset);
+
+                //arvore AVL alfab√©tica
+                EntradaRepositorio entrada_avl;
+                entrada_avl.palavra = strdup(token);
+                entrada_avl.frequencia = 1;
+                entrada_avl.offsets = malloc(sizeof(long));
+                entrada_avl.offsets[0] = offset;
+                entrada_avl.offset_cont = 1;
+
+                insere_avl_alfabeto(raiz_avl, entrada_avl);
             }
             token = strtok(NULL, " ");
         }
+
         free(info.citacao);
         free(info.filme);
+        free(info.ano);
+
         offset = ftell(arquivo);
     }
+
     fclose(arquivo);
 }
