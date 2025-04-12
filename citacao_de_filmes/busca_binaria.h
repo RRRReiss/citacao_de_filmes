cb1dcf5c96cef84f3939c28d9d00d14f73ab6f3b
evandroflausinoo <evandro.flausino@ufu.br>
2025-04-12 15:55:06 -0300

    Update busca_binaria.h

--- citacao_de_filmes/busca_binaria.h
+++ citacao_de_filmes/busca_binaria.h
@@ -3,15 +3,25 @@
 
 #include "citacoes.h"
 
+// estrutura que representa um vetor de entradas ordenadas alfabeticamente
 typedef struct VetorBuscaBinaria{
-    EntradaRepositorio *entradas;
-    int tamanho;
+    EntradaRepositorio *entradas; // ponteiro para o vetor de entradas
+    int tamanho; // quantidade de elementos no vetor
 } VetorBuscaBinaria;
 
+// realiza busca binaria em um vetor de entradas
 int busca_binaria(EntradaRepositorio *entradas, int qtd_entradas, char *palavra);
+
+// inicializa o vetor de busca binaria
 void inicializa_busbin(VetorBuscaBinaria *vetor);
+
+// libera toda a memoria usada pelo vetor
 void destroi_busbin(VetorBuscaBinaria *vetor);
+
+// insere uma entrada no vetor mantendo a ordem alfabetica
 void insere_busbin(VetorBuscaBinaria *vetor, EntradaRepositorio entrada);
+
+// busca uma palavra no vetor e retorna o ponteiro para a entrada, ou NULL se nao encontrar
 EntradaRepositorio *pesquisa_busbin(VetorBuscaBinaria *vetor, char *palavra);
 
 #endif
