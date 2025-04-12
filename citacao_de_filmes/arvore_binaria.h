a8de97de2ff2bf248d3d19594e1141725863ef0e
evandroflausinoo <evandro.flausino@ufu.br>
2025-04-12 15:50:01 -0300

    Update arvore_binaria.h

--- citacao_de_filmes/arvore_binaria.h
+++ citacao_de_filmes/arvore_binaria.h
@@ -3,15 +3,23 @@
 
 #include "citacoes.h"
 
+// nó da árvore binária de busca
 typedef struct NoArvoreBusca {
     EntradaRepositorio entrada;
     struct NoArvoreBusca *esq;
     struct NoArvoreBusca *dir;
 } NoArvoreBusca;
 
+// adiciona um offset a entrada (sem repetir)
 void add_offset_arvbus(EntradaRepositorio *entrada, long offset);
+
+// insere uma palavra na árvore
 void insere_arvbus(NoArvoreBusca **raiz, EntradaRepositorio entrada);
+
+// busca uma palavra na árvore
 EntradaRepositorio *pesquisa_arvbus(NoArvoreBusca *raiz, char *palavra);
+
+// libera a memória da árvore
 void destroi_arvbus(NoArvoreBusca *raiz);
 
 #endif
