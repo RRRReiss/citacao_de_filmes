d621ffb1ee6624212db9b52407849851ba365037
evandroflausinoo <evandro.flausino@ufu.br>
2025-04-11 21:33:47 -0300

    Update arvore_avl.h

--- citacao_de_filmes/arvore_avl.h
+++ citacao_de_filmes/arvore_avl.h
@@ -1,5 +1,5 @@
-#ifndef AVL_H
-#define AVL_H
+#ifndef ARVORE_AVL_H
+#define ARVORE_AVL_H
 
 #include "citacoes.h"
 
@@ -10,8 +10,9 @@ typedef struct NoAVL {
     int altura;
 } NoAVL;
 
-void insere_avl(NoAVL **raiz, const char *palavra, int cont, long offset);
+void insere_avl(NoAVL **raiz, EntradaRepositorio entrada);
 EntradaRepositorio *pesquisa_avl(NoAVL *raiz, const char *palavra);
+void em_ordem_avl(NoAVL *raiz);
 void destroi_avl(NoAVL *raiz);
 
 #endif
