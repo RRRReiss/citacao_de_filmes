29632e287e5710d279788dad805c551c85cc139f
evandroflausinoo <evandro.flausino@ufu.br>
2025-04-12 07:04:55 -0300

    Update arvore_avl.h

--- citacao_de_filmes/arvore_avl.h
+++ citacao_de_filmes/arvore_avl.h
@@ -10,15 +10,15 @@ typedef struct NoAVL {
     int altura;
 } NoAVL;
 
-// Para árvore alfabética
-void insere_avl_alfabeto(NoAVL **raiz, EntradaRepositorio entrada);
-EntradaRepositorio *pesquisa_avl_alfabeto(NoAVL *raiz, const char *palavra);
-
-// Para árvore de frequência
-void insere_avl_frequencia(NoAVL **raiz, EntradaRepositorio entrada);
-void em_ordem_frequencia(NoAVL *raiz);
-
-// Funções comuns
+NoAVL* cria_no_avl(EntradaRepositorio entrada);
+int altura(NoAVL *no);
+int fator_balanceamento(NoAVL *no);
+NoAVL* rotacao_direita(NoAVL *y);
+NoAVL* rotacao_esquerda(NoAVL *x);
+NoAVL* insere_avl(NoAVL **raiz, EntradaRepositorio entrada);
+EntradaRepositorio *pesquisa_avl_alfabeto(NoAVL *raiz, char *palavra);
 void destroi_avl(NoAVL *raiz);
+void em_ordem_frequencia(NoAVL *no);
+void insere_avl_frequencia(NoAVL **raiz, EntradaRepositorio entrada);
 
 #endif
